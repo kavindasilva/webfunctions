45828e335f224d33f3320d5dba9ceebe3561ff15
kavindasilva <kss.kavinda@gmail.com>
2019-01-13 18:58:45 +0530

    creating new Dir
    checking if directory exists
    type checking dir or file

--- fileHandling/scan.php
+++ fileHandling/scan.php
@@ -5,8 +5,33 @@
 */
 
 $scanPath="."; // current directory
-$files=scandir();
+$files=scandir($scanPath);
+$files=array_diff($files, array(".", "..") );
 
+foreach($files as $val){
+	if( is_dir($val) ){
+		echo "DIR: $val";
+	}
+	elseif( is_file($val) ){
+		echo "File: $val";
+	}
+	else{
+		echo "Neither Dir nor File: $val";
+	}
+	echo "<br/>";
+}
+echo "<hr/>";
+
+/**
+	Creates a new Directory If not exists
+*/
+$newDir="Dir2";
+if( !file_exists($newDir) ){
+	echo $newDir." not exists";
+	mkdir($newDir);
+}
+
+$files=array_diff($files, array(".", "..") );
 print_r($files);
 
 
