2f6fa19bdf6da011d5f736579eea93feaa333e53
kavindasilva <kss.kavinda@gmail.com>
2019-01-13 18:41:51 +0530

    file handling functions
    setup the environment

---
+++ fileHandling/scan.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+	This file does directory reading and identifying files, type, and file reading
+*/
+
+$scanPath="."; // current directory
+$files=scandir();
+
+print_r($files);
+
+
+
+?>
